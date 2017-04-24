0e8bc145fcc11a6f8de5a910796e7c1f1815d771
Hades\IXI <ixi150@gmail.com>
2017-04-24 21:17:15 +0200

    selection box

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -17,11 +17,13 @@ public class CameraControl : MonoBehaviour
     new Camera camera;
     Vector2 mousePos, mousePosScreen, keyboardInput, mouseScroll;
     bool isCursorInGameScreen;
+    Rect selectionRect, boxRect;
 
     private void Awake()
     {
         selectionBox = GetComponentInChildren<Image>(true).transform as RectTransform;
         camera = GetComponent<Camera>();
+        selectionBox.gameObject.SetActive(false);
     }
 
     private void Update()
@@ -71,7 +73,36 @@ public class CameraControl : MonoBehaviour
 
     void UpdateClicks()
     {
-        //todo
-        //selectionBox.anchoredPosition = mousePos;
+        if (Input.GetMouseButtonDown(0))
+        {
+            selectionBox.gameObject.SetActive(true);
+            selectionRect.position = mousePos;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            selectionBox.gameObject.SetActive(false);
+        }
+        if (Input.GetMouseButton(0))
+        {
+            selectionRect.size = mousePos - selectionRect.position;
+            boxRect = AbsRect(selectionRect);
+            selectionBox.anchoredPosition = boxRect.position;
+            selectionBox.sizeDelta = boxRect.size;
+        }
+    }
+
+    Rect AbsRect(Rect rect)
+    {
+        if (rect.width < 0)
+        {
+            rect.x += rect.width;
+            rect.width *= -1;
+        }
+        if (rect.height < 0)
+        {
+            rect.y += rect.height;
+            rect.height *= -1;
+        }
+        return rect;
     }
 }
