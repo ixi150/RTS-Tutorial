a6563e414446e4a9155913f3460f32da34cb0364
Hades\IXI <ixi150@gmail.com>
2017-04-27 10:45:21 +0200

    Shooting Soldiers

--- Assets/Scripts/Soldier.cs
+++ Assets/Scripts/Soldier.cs
@@ -5,6 +5,26 @@ using UnityEngine;
 
 public class Soldier : Unit, ISelectable
 {
+    [Header("Soldier")]
+    [Range(0, .3f), SerializeField]
+    float shootDuration = 0;
+    [SerializeField]
+    ParticleSystem muzzleEffect, impactEffect;
+    [SerializeField]
+    LayerMask shootingLayerMask;
+
+    LineRenderer lineEffect;
+    Light lightEffect;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        lineEffect = muzzleEffect.GetComponent<LineRenderer>();
+        lightEffect = muzzleEffect.GetComponent<Light>();
+        impactEffect.transform.SetParent(null);
+        EndShootEffect();
+    }
+
     public void SetSelected(bool selected)
     {
         healthBar.gameObject.SetActive(selected);
@@ -25,6 +45,49 @@ public class Soldier : Unit, ISelectable
 
     void Command(Dragon dragonToKill)
     {
-        //todo
+        target = dragonToKill.transform;
+        task = Task.chase;
+    }
+
+    public override void DealDamage()
+    {
+        if (Shoot())
+            base.DealDamage();
+    }
+
+    bool Shoot()
+    {
+        Vector3 start = muzzleEffect.transform.position;
+        Vector3 direction = transform.forward;
+
+        RaycastHit hit;
+        if (Physics.Raycast(start, direction, out hit, attackDistance, shootingLayerMask))
+        {
+            StartShootEffect(start, hit.point, true);
+            var unit = hit.collider.gameObject.GetComponent<Unit>();
+            return unit;
+        }
+        StartShootEffect(start, start + direction*attackDistance, false);
+        return false;
+    }
+
+    void StartShootEffect(Vector3 lineStart, Vector3 lineEnd, bool hitSomething)
+    {
+        if (hitSomething)
+        {
+            impactEffect.transform.position = lineEnd;
+            impactEffect.Play();
+        }
+        lineEffect.SetPositions(new Vector3[] { lineStart, lineEnd });
+        lineEffect.enabled = true;
+        lightEffect.enabled = true;
+        muzzleEffect.Play();
+        Invoke("EndShootEffect", shootDuration);
+    }
+
+    void EndShootEffect()
+    {
+        lightEffect.enabled= false;
+        lineEffect.enabled = false;
     }
 }
--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -20,22 +20,26 @@ public class Unit : MonoBehaviour
     public bool IsAlive { get { return hp > 0; } }
     public float HealthPercent { get { return hp / hpMax; } }
 
-    public Transform target;
-
-    [SerializeField]
-    float hp, hpMax = 100;
+    [Header("Unit")]
     [SerializeField]
     GameObject hpBarPrefab;
     [SerializeField]
-    float stoppingDistance = 1;
+    float hp, hpMax = 100;
+    [SerializeField]
+    protected float attackDistance = 1,
+        attackCooldown = 1,
+        attackDamage = 0,
+        stoppingDistance = 1;
 
+    protected Transform target;
     protected HealthBar healthBar;
     protected Task task = Task.idle;
     protected NavMeshAgent nav;
 
+    float attackTimer;
     Animator animator;
 
-    private void Awake()
+    protected virtual void Awake()
     {
         nav = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
@@ -76,10 +80,6 @@ public class Unit : MonoBehaviour
         nav.velocity = Vector3.zero;
     }
 
-    protected virtual void Attacking()
-    {
-        nav.velocity = Vector3.zero;
-    }
 
     protected virtual void Moving()
     {
@@ -104,9 +104,43 @@ public class Unit : MonoBehaviour
 
     protected virtual void Chasing()
     {
-        //todo
+        if (target)
+        {
+            nav.SetDestination(target.position);
+            float distance = Vector3.Magnitude(nav.destination - transform.position);
+            if (distance <= attackDistance)
+            {
+                task = Task.attack;
+            }
+        }
+        else
+        {
+            task = Task.idle;
+        }
     }
 
+    protected virtual void Attacking()
+    {
+        if (target)
+        {
+            nav.velocity = Vector3.zero;
+            transform.LookAt(target);
+            float distance = Vector3.Magnitude(nav.destination - transform.position);
+            if (distance <= attackDistance)
+            {
+                if ((attackTimer -= Time.deltaTime) <= 0)
+                    Attack();
+            }
+            else
+            {
+                task = Task.chase;
+            }
+        }
+        else
+        {
+            task = Task.idle;
+        }
+    }
 
     protected virtual void Animate()
     {
@@ -117,4 +151,29 @@ public class Unit : MonoBehaviour
         animator.SetBool(ANIMATOR_ALIVE, IsAlive);
     }
 
+    public virtual void Attack()
+    {
+        animator.SetTrigger(ANIMATOR_ATTACK);
+        attackTimer = attackCooldown;
+    }
+
+    public virtual void DealDamage()
+    {
+        if (target)
+        {
+            Unit unit = target.GetComponent<Unit>();
+            if (unit && unit.IsAlive)
+            {
+                unit.hp -= attackDamage;
+            }
+            else
+                target = null;
+        }
+    }
+
+    protected virtual void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackDistance);
+    }
 }
